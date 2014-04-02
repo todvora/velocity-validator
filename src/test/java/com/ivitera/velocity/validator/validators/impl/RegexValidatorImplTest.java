a98a662b35f3c4e211c992e60c5979605e980600
Tomas Dvorak <todvora@gmail.com>
2014-04-02 20:20:51 +0200

    regex validator test

---
+++ src/test/java/com/ivitera/velocity/validator/validators/impl/RegexValidatorImplTest.java
@@ -0,0 +1,38 @@
+package com.ivitera.velocity.validator.validators.impl;
+
+import com.ivitera.velocity.validator.exceptions.InitializationException;
+import com.ivitera.velocity.validator.exceptions.ValidationException;
+import com.ivitera.velocity.validator.validators.Validator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+public class RegexValidatorImplTest {
+
+    private Validator validator;
+
+    @Before
+    public void setup() throws InitializationException {
+        validator = new RegexValidatorImpl();
+        String filename = this.getClass().getResource("/properties.conf").getFile();
+        validator.init(new File(filename));
+    }
+
+    @Test
+    public void testCorrectTemplate() {
+        String file = this.getClass().getResource("/CorrectTemplate.vm").getFile();
+        try {
+            validator.validate(file);
+        } catch (Exception e) {
+            Assert.fail("Should not throw exception");
+        }
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testWrongForeach() throws Exception {
+        String file = this.getClass().getResource("/WrongForeachTemplate.vm").getFile();
+        validator.validate(file);
+    }
+}
